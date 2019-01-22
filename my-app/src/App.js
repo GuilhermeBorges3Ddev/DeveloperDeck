b87c44b096fe2b75775d0cf8c5ef4c53dddf2e49
GuilhermeBorges3Ddev <gasilvaborges@gmail.com>
2019-01-22 09:43:58 -0200

    Footer done and all the whitespaces were removed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,6 +7,10 @@ class App extends Component {
       <div className="container-fluid p-0">
           <nav className="navbar navbar-expand-md navbar-dark bg-dark">
             <a className="navbar-brand" href="#"><i className="fas fa-stroopwafel"></i>&nbsp;WD2019</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMainToggler"
+              aria-controls="navbarMainToggler" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
             <section className="collapse navbar-collapse" id="navbarMainToggler">
               <div className="navbar-nav ml-auto pr-3">
                 <a className="nav-item nav-link" href="#">Home</a>
@@ -18,23 +22,21 @@ class App extends Component {
                   <div className="input-group-prepend">
                     <span className="input-group-text">@</span>
                   </div>
-                  <input type="text" className="form-control mr-1" placeholder="Username"/>
-                  <button className="btn btn-outline-success">Login</button>  
+                  <input type="text" className="form-control mr-3" placeholder="Username"/>  
                 </div>
+                <button className="btn btn-outline-success">Login</button>
               </form>
-            </section>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMainToggler"
-              aria-controls="navbarMainToggler" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
+            </section>        
           </nav>
 
-          <footer className="container d-flex pt-3 justify-content-around footer bg-dark text-light text-center">
+          <footer className="container col-12 pt-3 footer bg-dark text-light text-center">
+            <container className="container col-6 col-md-2 d-flex justify-content-around">  
               <i class="fab fa-facebook-square"></i>
               <i class="fab fa-linkedin"></i>
               <i class="fab fa-google-plus-square"></i>
               <i class="fab fa-instagram"></i>
               <i class="fab fa-twitter-square"></i>
+            </container>  
           </footer>      
       </div>
     );
